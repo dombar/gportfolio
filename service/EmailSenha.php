d887ce5dcb378c652fdc240669d5a952f5af22d4
afranio <afranio@algartelecom.com.br>
2017-02-28 19:54:20 -0300

    Descriptografando senha

--- service/EmailSenha.php
+++ service/EmailSenha.php
@@ -16,8 +16,8 @@ if (isset($_POST['inputEmail'])) {
         if (isset($results)) {
             $nomeDestinatario  = $results->getUsuario_Nome();
 			$destinatario 	   = $results->getUsuario_Email();
-			$assunto	       = 'Recuperação de senha.';
-			$mensagem	       = 'Sua senha é: '.$results->getUsuario_Senha();
+			$assunto	       = 'Recuperaï¿½ï¿½o de senha.';
+            $mensagem	       = 'Sua senha ï¿½: '. md5_decrypt($results->getUsuario_Senha(),null);
 			$redirectPage      = '../index.php';
 			$enviaEmail        = new EmailService();
 			$enviaEmail        -> mail($destinatario, $assunto, $mensagem, $redirectPage, $nomeDestinatario);
@@ -31,6 +31,22 @@ if (isset($_POST['inputEmail'])) {
     catch (Exception $e) {
         echo $e->getMessage();
     }
+}
+
+function md5_decrypt($enc_text, $password, $iv_len = 16)
+{
+   $enc_text = base64_decode($enc_text);
+   $n = strlen($enc_text);
+   $i = $iv_len;
+   $plain_text = '';
+   $iv = substr($password ^ substr($enc_text, 0, $iv_len), 0, 512);
+   while ($i < $n) {
+	   $block = substr($enc_text, $i, 16);
+	   $plain_text .= $block ^ pack('H*', md5($iv));
+	   $iv = substr($block . $iv, 0, 512) ^ $password;
+	   $i += 16;
+   }
+   return preg_replace('/\\x13\\x00*$/', '', $plain_text);
 }
 
 ?>
